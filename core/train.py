4c0eba11737ec5f22248fc7991ed4fd3c8f97a88
VolkanB <112424491+VolB07@users.noreply.github.com>
2025-03-24 19:58:59 +0300

    update

--- core/train.py
+++ core/train.py
@@ -8,38 +8,40 @@ from tensorflow.keras.models import save_model
 
 from utils.prepare_datasets import PrepareDatasets
 from constants import Constants
+
+
 class Train:
-    def __init__(self,model_path,dataset_file_path):
-        self.model_path=model_path
+    def __init__(self, model_path, dataset_file_path):
+        self.model_path = model_path
         self.dataset_file_path = dataset_file_path
         self.prepare_datasets = PrepareDatasets(file_path=self.dataset_file_path)
-        self.crypto_prices=self.prepare_datasets.load_crypto_data_from_csv()
-        self.epochs=150
-        self.batch_size =64
+        self.crypto_prices = self.prepare_datasets.load_crypto_data_from_csv()
+        self.epochs = 150
+        self.batch_size = 64
         self.patience = 20
-    
-    
 
     def train(self):
-        X_train, y_train,X_test,y_test,X,y,train_size = self.prepare_datasets.split_train_test()
+        X_train, y_train, X_test, y_test, X, y, train_size = self.prepare_datasets.split_train_test()
 
         print("Kaydedilmiş model bulunamadı. Yeni bir model oluşturuluyor...")
         model = Sequential([
             LSTM(128, return_sequences=True, activation='tanh', input_shape=(
-                Constants.SEQUENCE_LENGTH, 1), 
-                kernel_regularizer=tf.keras.regularizers.l2(0.01)
-            ),
-            Dropout(0.3),  # Dropout oranı artırıldı
-            LSTM(64, return_sequences=False, 
-                 activation='tanh', 
+                Constants.SEQUENCE_LENGTH, 1),
                  kernel_regularizer=tf.keras.regularizers.l2(0.01)
-            ),
+                 ),
+            Dropout(0.3),  # Dropout oranı artırıldı
+            LSTM(64, return_sequences=False,
+                 activation='tanh',
+                 kernel_regularizer=tf.keras.regularizers.l2(0.01)
+                 ),
             Dropout(0.3),  # Dropout oranı artırıldı
             Dense(25, activation='relu'),
+            Dropout(0.3),  # Yeni Dropout katmanı eklendi
             Dense(1)
         ])
+
         model.compile(
-            optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), 
+            optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
             loss="mean_squared_error"
         )
 
@@ -52,14 +54,16 @@ class Train:
                             batch_size=self.batch_size,
                             validation_data=(X_test, y_test),
                             callbacks=[early_stopping]
-                )
+                            )
 
         # Modeli kaydet
         save_model(model, self.model_path)
         print("Model kaydedildi")
 
+
+# Eğitim başlat
 dataset_file_path = r"C:\Users\VolB\PycharmProjects\TradingAi\datasets\btc_15m_data_2018_to_2025.csv"
 Train(
-    model_path= os.path.join("models","bitcoin_price_lstm_15m_model1.h5"),
+    model_path=os.path.join("models", "bitcoin_price_lstm_15m_model1.h5"),
     dataset_file_path=dataset_file_path
-    ).train()
+).train()
